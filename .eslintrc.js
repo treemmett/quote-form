12f6772bcc1d19caada202d1ab9ddbca4139ade1
treemmett <tregan@tregan.me>
2019-05-04 18:26:05 -0600

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+    '@vue/typescript',
+    'plugin:prettier/recommended',
+    'prettier/vue'
+  ],
+  rules: {
+    'class-methods-use-this': 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-param-reassign': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        htmlWhitespaceSensitivity: 'ignore',
+        singleQuote: true
+      }
+    ]
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  }
+};
